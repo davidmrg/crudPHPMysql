45525ef7e2d4ca7f0642d04ecf17c2a38d49c4cd
David Ramirez <david.ramirez@ar.ibm.com>
2020-03-24 11:52:02 -0300

    Release1: Sistema CRUD básico en PHP

---
+++ save.php
@@ -0,0 +1,29 @@
+<?php include("includes/header.php")?>
+<?php include("includes/footer.php")?>
+<?php include("db.php")?>
+<?php 
+
+if (isset($_POST['guardar_registro'])){
+    $nombres = $_POST['nombres'];
+    $direccion = $_POST['direccion'];
+    //echo $nombres;
+    //echo $direccion;
+}
+
+    $query = "INSERT INTO alumno(nombres, direccion) VALUES ('$nombres','$direccion')";
+    $result = mysqli_query($conn, $query);
+    if(!$result){
+        die("Fallo en el query. Existe un problema en la base de datos.");
+    }
+    else{
+        ?>
+        <script>alert("Registro Guardado");</script>
+        <?php 
+        
+    }
+
+    //si quisiera redireccionar a index directamente:
+    ?>
+    <script>
+    window.location = "index.php";
+    </script>
